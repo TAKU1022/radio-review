9de594a8cbbc7c5ef99ecca44ff5cb3d33ee8b8e
Takumi <nase49946@gmail.com>
2022-04-20 16:36:28 +0900

    feat: 認証ガード追加

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,12 +1,15 @@
 import { NextPage } from 'next';
+import { GuestGuard } from '../../components/functional/GuestGuard';
 import { CommonLayout } from '../../components/layout/CommonLayout';
 import { Login } from '../../components/page/Login';
 
 const LoginPage: NextPage = () => {
   return (
-    <CommonLayout>
-      <Login />
-    </CommonLayout>
+    <GuestGuard>
+      <CommonLayout>
+        <Login />
+      </CommonLayout>
+    </GuestGuard>
   );
 };
 
